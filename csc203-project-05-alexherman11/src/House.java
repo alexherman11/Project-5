7d3695d2eb85ec7586d94b5304417c2701140698
Alex Herman <1wheelalex@gmail.com>
2023-03-18 21:42:51 -0700

    ??

--- csc203-project-05-alexherman11/src/House.java
+++
@@ -1,9 +0,0 @@
-import processing.core.PImage;
-
-import java.util.List;
-
-public class House extends Entity{
-    public House (String id, Point position, List<PImage> images) {
-        super(id, position, images);
-    }
-}
